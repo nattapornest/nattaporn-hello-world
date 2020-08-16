cdd7d55d37c1b71bde865df574b780c010151d5d
Natchanon002 <69023064+Natchanon002@users.noreply.github.com>
2020-08-16 11:36:07 +0700

    สร้าง superclass ชื่อ animal

--- day2/src/main.kt
+++ day2/src/main.kt
@@ -1,5 +1,22 @@
 fun main(){
 
+}
+open class Animal{
+    val image = ""
+    val food = ""
+    val habitat = "" //ที่อยู่อาศัย
+    var hunger = 10  //กินเยอะไหม
 
-
+   open fun makeNoise () {
+        println("The Animal is making a noise")
+    }
+   open fun eat () {
+        println("The Animal is eating")
+    }
+   open fun roam(){
+        println("The Animal is roaming")
+    }
+    fun sleep(){
+        println("The Animal is sleeping")
+    }
 }
