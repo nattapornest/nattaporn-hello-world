a678dfd5e0e9187f6dd3186b824e94f78baffc9e
Natchanon002 <69023064+Natchanon002@users.noreply.github.com>
2020-08-16 12:18:56 +0700

    สร้าง override funtion ให้กับ Hippo and cat

--- day2/src/main.kt
+++ day2/src/main.kt
@@ -1,11 +1,21 @@
 fun main(){
-var hip = Hippo()
-    hip.eat()
+var myhip = Hippo()
+    myhip.eat()
+    myhip.roam()
+    myhip.makeNoise()
+    myhip.sleep()
+
+    //สร้างแมว
+    var mycat = Cat()
+    mycat.eat()
+    mycat.roam()
+    mycat.makeNoise()
+    mycat.sleep()
 }
 open class Animal{
-    val image = ""
-    val food = ""
-    val habitat = "" //ที่อยู่อาศัย
+    open val image = ""
+   open val food = ""
+   open val habitat = "" //ที่อยู่อาศัย
     var hunger = 10  //กินเยอะไหม
 
    open fun makeNoise () {
@@ -23,4 +33,28 @@ open class Animal{
 }
 class Hippo : Animal(){
     //สร้างคลาสฮิปโปแต่ยังไม่ได้ทำอะไร
+    override val image = "hippo.jpg"
+    override val food = "grass"
+    override val habitat = "water"
+
+    override fun makeNoise() {
+        println("Grunt! Grunt!")
+    }
+
+    override fun eat() {
+        println("The Hippo is eating $food")
+    }
+}
+class Cat : Animal(){
+    override val image = "cat.jpg"
+    override val food = "rat"
+    override val habitat = "Home"
+
+    override fun makeNoise() {
+        println("เหมียว เหมียว")
+    }
+
+    override fun eat() {
+        println("The Cat is eating $food")
+    }
 }
